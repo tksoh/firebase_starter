e64a9185c3666464bbdb4990d75e7cef130eaa87
TK Soh <teekaysoh@gmail.com>
2025-03-29 17:32:26 +0800

    home: add action togle list/grid view

--- lib/main.dart
+++ lib/main.dart
@@ -41,6 +41,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int userCount = 0;
+  bool toggleView = false;
 
   @override
   Widget build(BuildContext context) {
@@ -48,10 +49,19 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+        actions: [
+          IconButton(
+            onPressed: () {
+              setState(() {
+                toggleView = !toggleView;
+              });
+            },
+            icon: Icon(toggleView ? Icons.list : Icons.grid_4x4_outlined),
+          )
+        ],
       ),
       drawer: MyDrawer(),
-      body: MyUserListView(),
-      // body: MyUserGridView(),
+      body: toggleView ? MyUserGridView() : MyUserListView(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           userCount++;
